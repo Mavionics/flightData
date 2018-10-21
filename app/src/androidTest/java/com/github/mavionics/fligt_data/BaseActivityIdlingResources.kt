9d953648efe38f85c2b212fbe7d54de3256c69aa
Alexander Poole <alex.o.poole@gmail.com>
2018-10-21 08:15:08 +0200

    Add fail to sign in test

---
+++ app/src/androidTest/java/com/github/mavionics/fligt_data/BaseActivityIdlingResources.kt
@@ -0,0 +1,38 @@
+package com.github.mavionics.fligt_data
+
+import android.app.ProgressDialog
+import android.support.test.espresso.IdlingResource
+
+import com.github.mavionics.fligt_data.BaseActivity
+import  com.github.mavionics.fligt_data.EmailPasswordActivity
+/**
+ * Monitor Activity idle status by watching ProgressDialog.
+ */
+class BaseActivityIdlingResource : IdlingResource {
+
+    private var mActivity: BaseActivity? = null
+    private var mCallback: IdlingResource.ResourceCallback? = null
+
+    constructor(activity: EmailPasswordActivity) {
+        mActivity = activity
+    }
+
+    override fun getName(): String {
+        return "BaseActivityIdlingResource:" + mActivity!!.localClassName
+    }
+
+    override fun isIdleNow(): Boolean {
+        val dialog = mActivity!!.progressDialog
+        val idle = dialog == null || !dialog!!.isShowing()
+
+        if (mCallback != null && idle) {
+            mCallback!!.onTransitionToIdle()
+        }
+
+        return idle
+    }
+
+    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback) {
+        mCallback = callback
+    }
+}
