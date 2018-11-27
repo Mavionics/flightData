dca06e322d6cf17983ab34e8ea74fba81c118fd4
Alexander Poole <alex.o.poole@gmail.com>
2018-11-27 18:23:54 +0100

    Get vehicles from firestore

--- app/src/main/java/com/github/mavionics/fligt_data/MainActivity.java
+++ app/src/main/java/com/github/mavionics/fligt_data/MainActivity.java
@@ -31,20 +31,12 @@ import android.view.View;
 import com.github.mavionics.fligt_data.fragment.MyPostsFragment;
 import com.github.mavionics.fligt_data.fragment.MyTopPostsFragment;
 import com.github.mavionics.fligt_data.fragment.MyVehiclesFragment;
-import com.github.mavionics.fligt_data.fragment.RecentPostsFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class  MainActivity extends BaseActivity {
 
@@ -101,24 +93,21 @@ public class  MainActivity extends BaseActivity {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        Log.d(TAG, "Starting to collect ");
-
-        DocumentReference docRef = db.collection("vehicles").document("jR1GKJMHCXyX22x1VklE");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                    } else {
-                        Log.d(TAG, "No such document");
+        db.collection("vehicles")
+                .whereEqualTo("owner",FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
                     }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
+                });
 
     }
 
