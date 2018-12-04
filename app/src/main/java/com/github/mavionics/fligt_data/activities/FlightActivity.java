2a36abe78e06ef48df4e30fea18ced7791b3093a
Alexander Poole <alex.o.poole@gmail.com>
2018-12-04 21:07:44 +0100

    Feature/test login (#22)
    * Clean up login activity and change logo
    
    * Always show loading dialog before atempting to login
    
    * Continously update position of vehicle
    
    * Make ui for FlightActivity

--- app/src/main/java/com/github/mavionics/fligt_data/activities/FlightActivity.java
+++ app/src/main/java/com/github/mavionics/fligt_data/activities/FlightActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.github.mavionics.fligt_data.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,28 +23,71 @@ import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DecimalFormat;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FlightActivity extends BaseActivity {
 
-    private FirebaseFirestore db;
-    private Location location;
+    private FirebaseFirestore mDatabase;
+    private Location mLocation;
     private String TAG = "FlightActivity";
+    private String mVehicleName;
+    private LocationManager mLocationManager;
+    private GeoPoint mPosition;
+    private Timestamp mLastUpdated;
+
+    // Init
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateLocation();
+            if(mHandler != null)
+                mHandler.postDelayed(this, 1000);
+        }
+    };
+
+    @BindView(R.id.vehicleName) TextView mNameView;
+    @BindView(R.id.vehiclePosition) TextView mPositionView;
+    @BindView(R.id.vehicleLastUpdated) TextView mLastUpdatedView;
+    private DecimalFormat mDecimalFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        final String message =
-                intent.getStringExtra("VEHICLE_NAME");
+        mVehicleName = intent.getStringExtra("VEHICLE_NAME");
+        mNameView.setText(mVehicleName);
 
-        Log.d(TAG, "onCreate: " + message);
+        Log.d(TAG, "onCreate: vehicle name: " + mVehicleName);
 
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        // Update location every second
+        mHandler.postDelayed(mRunnable, 1000);
+
+        mDecimalFormat = new DecimalFormat("#.000");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+        mHandler = null;
+        finish();
+    }
+
+    private void updateLocation(){
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this,
@@ -50,15 +95,10 @@ public class FlightActivity extends BaseActivity {
             showPhoneStatePermission();
             return;
         }
+        mLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        mDatabase = FirebaseFirestore.getInstance();
 
-        location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-
-        // Access a Cloud Firestore instance from your Activity
-
-        db = FirebaseFirestore.getInstance();
-
-        db.collection("vehicles")
+        mDatabase.collection("vehicles")
                 .whereEqualTo("owner",getUid())
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -66,16 +106,19 @@ public class FlightActivity extends BaseActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
 
                                 Map<String, Object> vehicle = document.getData();
-                                Log.d(TAG, "vehicle name: " + vehicle.get("name"));
-                                if(vehicle.get("name").equals(message)){
-                                    Log.d(TAG, "onComplete: matches activity name");
-                                    vehicle.put("position", new GeoPoint(location.getLatitude(), location.getLongitude()));
-                                    vehicle.put("timestamp", Timestamp.now());
+                                if(vehicle.get("name").equals(mVehicleName)){
+                                    mPosition = new GeoPoint(mLocation.getLatitude(), mLocation.getLongitude());
+                                    mLastUpdated = Timestamp.now();
+                                    vehicle.put("position", mPosition);
+                                    vehicle.put("timestamp", mLastUpdated);
                                     // Add a new document with a generated ID
-                                    db.collection("vehicles").document(document.getId()).update(vehicle);
+
+                                    Log.d(TAG, "Updating vehicle location: " + vehicle.toString());
+                                    mDatabase.collection("vehicles").document(document.getId()).update(vehicle);
+                                    mPositionView.setText(GeoToString(mPosition));
+                                    mLastUpdatedView.setText(mLastUpdated.toDate().toString());
                                 }
                             }
                         } else {
@@ -84,6 +127,9 @@ public class FlightActivity extends BaseActivity {
                     }
                 });
     }
-
+    private String GeoToString(GeoPoint position){
+        return "lat: " + mDecimalFormat.format(position.getLatitude()) +
+                ", lon: " + mDecimalFormat.format(position.getLongitude());
+    }
 
 }
