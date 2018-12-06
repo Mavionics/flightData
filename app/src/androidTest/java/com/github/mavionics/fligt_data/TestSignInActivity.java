2f8ff087ad5c44e640ffdde56c7e79666d5a41d4
Alexander Poole <alex.o.poole@gmail.com>
2018-12-06 20:20:14 +0100

    Add test classes

---
+++ app/src/androidTest/java/com/github/mavionics/fligt_data/TestSignInActivity.java
@@ -0,0 +1,28 @@
+package com.github.mavionics.fligt_data;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.github.mavionics.fligt_data.activities.SignInActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class TestSignInActivity {
+
+    @Rule
+    public ActivityTestRule<SignInActivity> activityActivityTestRule = new ActivityTestRule<SignInActivity>(SignInActivity.class);
+
+    @Test
+    public void TestFaultySignIn(){
+        onView(withId(R.id.fieldEmail)).check(matches((isDisplayed())));
+    }
+
+}
