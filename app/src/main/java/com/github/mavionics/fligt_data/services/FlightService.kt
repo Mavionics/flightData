5e40950ab9a444757f5bc3f687181ed21fadf986
Alexander Poole <alex.o.poole@gmail.com>
2018-12-11 20:17:16 +0100

    Add flightService

---
+++ app/src/main/java/com/github/mavionics/fligt_data/services/FlightService.kt
@@ -0,0 +1,12 @@
+package com.github.mavionics.fligt_data.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FlightService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
